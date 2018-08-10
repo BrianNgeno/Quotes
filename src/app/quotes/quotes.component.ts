850c346c7ad88656f8a1f4ccaa6f798b2a5579c5
brianngeno <bkn.ngeno@gmail.com>
2018-08-10 12:23:27 +0300

    added the date property to the quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,9 +7,9 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quotes = [
-    new Quote(1, 'Start Early finish Early', 'By Martin Luther'),
-    new Quote(2, 'The mind is the greatest weapon against poverty', 'By marvin Gavin'),
-    new Quote(3, 'Never underestimate the power of Confidence', 'By Lincon Martin'),
+    new Quote(1, 'Start Early finish Early', 'By Martin Luther', new Date(2018, 9, 14)),
+    new Quote(2, 'The mind is the greatest weapon against poverty', 'By marvin Gavin', new Date(2018, 4, 13)),
+    new Quote(3, 'Never underestimate the power of Confidence', 'By Lincon Martin', new Date(2018, 5, 19)),
   ];
 
   constructor() { }
