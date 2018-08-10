456c650e35f9afa079e0b642524fd30491a6125f
brianngeno <bkn.ngeno@gmail.com>
2018-08-10 07:41:28 +0300

    added an array to to component quotes.ts to store the properties of the quotes and updated component quotes.html to display the properties

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes = [
+    new Quote(1, 'Start Early finish Early'),
+    new Quote(2, 'The mind is the greatest weapon against poverty'),
+    new Quote(3, 'Never underestimate the power of Confidence'),
+  ];
 
   constructor() { }
 
